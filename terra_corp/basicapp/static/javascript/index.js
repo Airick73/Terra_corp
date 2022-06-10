d8838338e12b630f7edaa0f7d905bab9ef21894a
JMarshalB <jaylenbrown2823@outlook.com>
2022-06-09 21:48:48 -0400

    navbar update

--- terra_corp/basicapp/static/javascript/index.js
+++ terra_corp/basicapp/static/javascript/index.js
@@ -1,1 +1,51 @@
+var currentWindowPOS = 0;
+var lastWindowPOS = 0;
+var ticking = false;
 
+window.addEventListener("scroll", function () {
+    currentWindowPOS = window.scrollY;
+
+    if(!ticking) {
+        window.requestAnimationFrame(function (){
+            animateNavbar(currentWindowPOS);
+            ticking = false;
+        });
+    }
+
+    else {
+        ticking = true;
+    }
+
+});
+
+function animateNavbar(position) {
+    if(currentWindowPOS > lastWindowPOS){
+      setTimeout(() => {
+        $(".navbar").slideUp("fast");
+      },"150");
+    }
+    else if(currentWindowPOS === lastWindowPOS){
+
+    }
+    else {
+      if(position === 0){
+        $(".navbar").removeClass("alt-navbar-bg");
+        $(".navbar-brand, .nav-link").removeClass("alt-navbar-text");
+        setTimeout(() => {
+          $(".navbar").slideDown("fast");
+        },"150");
+
+      }
+
+      else {
+        $(".navbar").addClass("alt-navbar-bg");
+        $(".navbar-brand, .nav-link").addClass("alt-navbar-text");
+        setTimeout(() => {
+          $(".navbar").slideDown("fast");
+        },"200");
+
+      }
+    }
+
+    lastWindowPOS = currentWindowPOS;
+}
