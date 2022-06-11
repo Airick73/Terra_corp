b21c68c97003616f4a29851530539818070febb3
JMarshalB <jaylenbrown2823@outlook.com>
2022-06-11 17:42:46 -0400

    Navbar Update; Refactoring; Mobile update
    Resolved some navbar bugs, updated about-me and careers page, made it more mobile friendly.

--- terra_corp/basicapp/static/javascript/about_me.js
+++ terra_corp/basicapp/static/javascript/about_me.js
@@ -1,3 +1,3 @@
-//BG for Navbar//
+$('.menu-icon').attr("src", "../../static/images/menu-dark.svg");
 $('body').css("background-color", 'white');
 $('.navbar-brand, nav-link').css("color", 'black');
--- terra_corp/basicapp/static/javascript/carrers.js
+++ terra_corp/basicapp/static/javascript/carrers.js
@@ -1,2 +1,3 @@
+$('.menu-icon').attr("src", "../../static/images/menu-dark.svg");
 $('body').css("background-color", 'white');
 $('.navbar-brand, nav-link').css("color", 'black');
