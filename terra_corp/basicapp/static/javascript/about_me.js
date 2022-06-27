50a261ff9e470fb0878738a57bb3b4d76815d10f
JMarshalB <jaylenbrown2823@outlook.com>
2022-06-27 15:54:08 -0400

    Added about me content to main page

--- terra_corp/basicapp/static/javascript/about_me.js
+++
@@ -1,4 +0,0 @@
-$(".navbar").addClass("navbar-static");
-$('.menu-icon').attr("src", "../../static/images/menu-dark.svg");
-$('body').css("background-color", 'white');
-$('.navbar-brand, nav-link').css("color", 'black');
