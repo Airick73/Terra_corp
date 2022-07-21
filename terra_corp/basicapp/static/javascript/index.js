b051556ed372a724c2cd6ca5da02cf0fed2bae9a
JMarshalB <jaylenbrown2823@outlook.com>
2022-07-21 19:10:52 -0400

    News Card Update, Title Section Update

--- terra_corp/basicapp/static/javascript/index.js
+++ terra_corp/basicapp/static/javascript/index.js
@@ -5,3 +5,19 @@ $(':root').css("--windowHeight", $( window ).height() + 'px');
 window.addEventListener('resize', () => {
   $(':root').css("--windowHeight", $( window ).height() + 'px');
 });
+
+var elementID = [];
+Array.from(document.getElementsByClassName("news-link")).forEach( (name) => {
+  elementID.push("#" + name.getAttribute("id"));
+});
+
+elementID.forEach((ID) => {
+  $(ID).hover( () => {
+    $(ID + " img").addClass("img-hover");
+    $(ID + " .img-overlay").addClass("img-overlay-hover");
+  }, () => {
+    $(ID + " img").removeClass("img-hover");
+    $(ID + " .img-overlay").removeClass("img-overlay-hover");
+  });
+});
+
